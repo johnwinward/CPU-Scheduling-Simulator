cf1e59d807ddcbe7005f49e49810470939717e2b
John Winward <john.winward2@gmail.com>
2022-02-27 16:16:41 -0500

    Added default constructor implementing Random, toString, static variable n (counts instances), and constants.

--- src/PCB.java
+++ src/PCB.java
@@ -1,13 +1,35 @@
 //Process Control Block
 
-public class PCB {
+import java.util.Random;
 
+public class PCB {
+    //Constants
+    private static final int MAX_BURST_TIME = 1000;
+    private static final int MAX_SIM_TIME = 100000;
+
+    //Process Count (will increase with each instance created)
+    private static int n = 0;
+
+    //Random Number Generator
+    private static Random r = new Random();
+
+    //Instance variables
     private String name;
     private int processID;
     private int burstTime;
     private int arrivalTime;
     private int priority;
 
+    //Default Constructor
+    PCB(){
+        name = "Process " + n;
+        processID = n++;
+        burstTime = r.nextInt(0, MAX_BURST_TIME);
+        arrivalTime = r.nextInt(MAX_SIM_TIME);
+        priority = 0; //priority will be assigned by scheduling algorithm
+    }
+
+    //Manually Assigned Constructor
     PCB(String name, int processID, int burstTime, int arrivalTime){
         this.name = name;
         this.processID = processID;
@@ -54,5 +76,16 @@ public class PCB {
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "PCB{" +
+                "name='" + name + '\'' +
+                ", processID=" + processID +
+                ", burstTime=" + burstTime +
+                ", arrivalTime=" + arrivalTime +
+                ", priority=" + priority +
+                '}';
     }
 }
